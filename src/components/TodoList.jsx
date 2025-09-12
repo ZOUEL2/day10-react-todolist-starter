84895f0a438186e88945e45a2a98960552a4af97
ZOUEL2 <zouel2@oocl.com>
2025-09-12 15:10:05 +0800

    refactor: update TodoList component to safely access context and handle empty state

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,17 @@
-const TodoList = () => {
-  return (
-      <div>This is the TodoList Component.</div>
-  );
-}
+import { useContext } from "react";
+import { TodoContext } from "../contexts/TodoContext";
 
-export default TodoList
+const TodoList = () => {
+  const { state, dispatch } = useContext(TodoContext);
+
+  return (
+    <div className="todo-list">
+      <div>This is the TodoList Component.</div>
+      {state.map((todo) => {
+        return <div className="todo-item">{todo.text} </div>;
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
